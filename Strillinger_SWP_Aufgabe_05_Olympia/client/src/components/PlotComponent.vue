94cc1ca3883d41669479a246c5dec3ce6a7fb95d
Josef Strillinger <jstrillinger@outlook.com>
2023-02-04 22:48:35 +0100

    v1.1

---
+++ Strillinger_SWP_Aufgabe_05_Olympia/client/src/components/PlotComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <VuePlotly :data="p2" :layout="layout"></VuePlotly>
+</template>
+
+<script setup>
+import { VuePlotly } from "vue3-plotly";
+</script>
---
+++ Strillinger_SWP_Aufgabe_05_Olympia/client/src/components/TableComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <DataTable
+    class="table table-hover table-striped"
+    width="100%"
+    :data="medals"
+  >
+    <thead>
+      <tr>
+        <th v-for="c in columns">{{ c }}</th>
+      </tr>
+    </thead>
+    <tfoot></tfoot>
+  </DataTable>
+</template>
+
+<script>
+import DataTable from "datatables.net-vue3";
+import DataTablesLib from "datatables.net-bs5";
+
+export default {
+  components: {
+    DataTable,
+  },
+  setup() {
+    console.log("setup");
+    DataTable.use(DataTablesLib);
+  },
+};
+</script>
+
+<style scoped>
+@import "bootstrap";
+@import "datatables.net-bs5";
+</style>
